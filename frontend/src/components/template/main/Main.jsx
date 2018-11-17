4bdd9e1f03414dc88653ba5d49e40a9cceaaad2a
Erislandio Soares <erislandiosoares21@gmail.com>
2018-11-17 20:00:03 -0200

    Adição das funções de crud do sistema

--- frontend/src/components/template/main/Main.jsx
+++ frontend/src/components/template/main/Main.jsx
@@ -7,10 +7,13 @@ class Main extends Component {
     render() {
         return (
             <React.Fragment>
-                <Header {...this.props}/>
+                <Header {...this.props} />
                 <main className="content container-fluid">
                     <div className="p-3 mt-3">
                         {this.props.children}
+                        <p className="mt-3">
+                            {this.props.subtitle}
+                        </p>
                     </div>
                 </main>
             </React.Fragment>
